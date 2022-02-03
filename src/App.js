356d8dd804aea6df8a5b9552c135bf58f90a6c51
Despat Tanguy <tanguydespat@gmail.com>
2022-02-03 16:34:13 +0100

    fso exo step 5

--- src/App.js
+++ src/App.js
@@ -2,29 +2,32 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  // const definitions
-  const course = 'Half Stack application development'
-  const title = 'FullStackOpen React Exercise 1'
-  
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+
+  const course = {
+    name: 'Half Stack application development',
+    title: 'FullStackOpen React Exercise 1',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      },
+    ],
   }
 
-  const total = part1.exercises + part2.exercises + part3.exercises
+  const total = course.parts.reduce((total, part) => total + part.exercises, 0)
 
   return (
     <div>
-      <Header title={title} course={course} />
-      <List part1={part1} part2={part2} part3={part3} />
+      <Header course={course} />
+      <List parts={course.parts} />
       <Total total={total} />
     </div>
   )
@@ -33,8 +36,8 @@ const App = () => {
 const Header = (props) => {
   return (
     <div className="header">
-      <h1>{props.title}</h1>
-      <h2>{props.course}</h2>
+      <h1>{props.course.title}</h1>
+      <h2>{props.course.name}</h2>
     </div>
   )
 }
@@ -44,9 +47,9 @@ const List = (props) => {
     <div className="list">
       <h2>List of exercises</h2>
       <ul>
-        <Part name={props.part1.name} number={props.part1.exercises} />
-        <Part name={props.part2.name} number={props.part2.exercises} />
-        <Part name={props.part3.name} number={props.part3.exercises} />
+        <Part name={props.parts[0].name} number={props.parts[0].exercises} />
+        <Part name={props.parts[1].name} number={props.parts[1].exercises} />
+        <Part name={props.parts[2].name} number={props.parts[2].exercises} />
       </ul>
     </div>
   )
@@ -68,7 +71,5 @@ const Total = (props) => {
     </div>
   )
 }
-
-
 
 export default App
